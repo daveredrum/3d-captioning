fb83db1df66d0c3438ea337ac9c1dddc091b75c4
Dave Chen <zhenyu.chen@campus.lmu.de>
2018-04-15 18:29:34 +0200

    modified preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -7,14 +7,15 @@ def preprocess(csv_file):
     warnings.simplefilter('ignore')
     # convert to lowercase
     csv_file.description = csv_file.description.str.lower()
-    # padding before all punctuations
-    # it takes some time
+    # preprocess
     captions_list = csv_file.description.values.tolist()
     for i in range(len(captions_list)):
         try:
+            # padding before all punctuations
             caption = captions_list[i]
             caption = re.sub(r'([.,!?()])', r' \1 ', caption)
             caption = re.sub(r'\s{2,}', ' ', caption)
+            # add end symbol
             caption += ' <END>'
             captions_list[i] = caption
         except Exception:
