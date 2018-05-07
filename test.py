318026646bb889b74b00ebe7e60a4f9b3075f937
Dave Chen <zhenyu.chen@campus.lmu.de>
2018-05-07 23:33:00 +0200

    modified test

--- test.py
+++ test.py
@@ -112,7 +112,7 @@ def main(args):
         for i in range(len(descriptions[phase])):
             plt.subplot(len(descriptions[phase]), 1, i+1)
             if model_type == "2d":
-                plt.imshow(Image.fromarray(images[phase][i].numpy()).resize((64, 64)))
+                plt.imshow(transforms.ToPILImage(images[phase][i]).view(3, 64, 64))
             elif model_type == "3d":
                 plt.imshow(Image.open(images[phase][i]).resize((64, 64)))
             plt.text(80, 32, descriptions[phase][i], fontsize=12)
