a308207a9a7099b316c2bd2c524e280a24b7c426
Dave Chen <zhenyu.chen@campus.lmu.de>
2018-05-09 00:25:12 +0200

    modified solver

--- solver.py
+++ solver.py
@@ -323,7 +323,6 @@ class EncoderDecoderSolver():
             # evaluate bleu
             eval_since = time.time()
             print(references["train"].keys())
-            print(candidates["train"].keys())
             train_blue, _ = capbleu.Bleu(4).compute_score(references["train"], candidates["train"])
             valid_blue, _ = capbleu.Bleu(4).compute_score(references["valid"], candidates["valid"])
             # evaluate cider
