efb3f0f7f1fb30ffa9d69e4738712998f34b82d5
Dave Chen <zhenyu.chen@campus.lmu.de>
2018-05-09 00:34:55 +0200

    modified solver

--- solver.py
+++ solver.py
@@ -310,9 +310,9 @@ class EncoderDecoderSolver():
                         # save to candidates
                         for model_id, output in zip(model_ids, outputs):
                             if model_id not in candidates[phase].keys():
-                                candidates[phase][model_id.numpy()] = [output]
+                                candidates[phase][model_id.numpy()[0]] = [output]
                             else:
-                                candidates[phase][model_id.numpy()].append(output)
+                                candidates[phase][model_id.numpy()[0]].append(output)
 
                         # save log
                         log['valid_loss'].append(loss.data[0])
