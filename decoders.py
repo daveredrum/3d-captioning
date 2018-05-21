ad7c58737018b1d1547119f68ee3f2fe67e18f13
Dave Chen <zhenyu.chen@campus.lmu.de>
2018-05-21 17:06:54 +0200

    modified decoder

--- decoders.py
+++ decoders.py
@@ -171,10 +171,10 @@ class AttentionDecoder2D(nn.Module):
         batch_size = visual_inputs.size(0)
         decoder_outputs = []
         for step in range(seq_length):
-            print(states[0][0].max(1)[0])
             # get the attention weights
             # attended = (batch_size, hidden_size)
             attention_weights = self.attend(visual_inputs, states)
+            print(attention_weights)
             attended = torch.matmul(
                 visual_inputs.view(batch_size, self.visual_channels, self.visual_flat),
                 attention_weights.view(batch_size, self.visual_flat, 1)    
