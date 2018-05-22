335612194b52c5e4123fba08b6d810bed3c0b029
Dave Chen <zhenyu.chen@campus.lmu.de>
2018-05-22 23:49:26 +0200

    modified decoder

--- decoders.py
+++ decoders.py
@@ -95,8 +95,8 @@ class Attention2D(nn.Module):
         hidden = (hidden - hidden_min) / (hidden_max - hidden_min)
         V = torch.matmul(visual_inputs, self.w_v) + self.b_v
         H = torch.matmul(hidden, self.w_h) + self.b_h
-        # print(V[0].min(0)[0].item(), V[0].max(0)[0].item())
-        # print(H[0].min(0)[0].item(), H[0].max(0)[0].item())
+        print(V[0].min(0)[0].item(), V[0].max(0)[0].item())
+        print(H[0].min(0)[0].item(), H[0].max(0)[0].item())
         # combine
         outputs = torch.matmul(torch.cat((V, H), dim=1), self.w_o) + self.b_o
         # outputs = (batch_size, output_size)
