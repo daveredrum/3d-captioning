858f17e822533fd215ec44c54c639d2460c848bc
Dave Chen <zhenyu.chen@campus.lmu.de>
2018-06-05 22:57:12 +0200

    modified solver

--- solver.py
+++ solver.py
@@ -651,7 +651,12 @@ class EncoderDecoderSolver():
                     eta_m,
                     eta_s - eta_m * 60)
                 )
-                print("[Debug] image_id: {}\n[Debug] val_ref: {}\n[Debug] val_can: {}\n".format(
+                print("[Debug] train_id: {}\n[Debug] train_ref: {}\n[Debug] train_can: {}".format(
+                    list(references["train"].keys())[0],
+                    references["train"][list(references["train"].keys())[0]][0],
+                    candidates["train"][list(references["train"].keys())[0]][0]
+                ))
+                print("[Debug] val_id: {}\n[Debug] val_ref: {}\n[Debug] val_can: {}\n".format(
                     list(references["val"].keys())[0],
                     references["val"][list(references["val"].keys())[0]][0],
                     candidates["val"][list(references["val"].keys())[0]][0]
