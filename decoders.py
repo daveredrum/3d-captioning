a6a574a1f555f331a66cba01dbe09c3c139d099c
Dave Chen <zhenyu.chen@campus.lmu.de>
2018-06-09 11:50:33 +0200

    modified beam search

--- decoders.py
+++ decoders.py
@@ -97,8 +97,9 @@ class Decoder(nn.Module):
                     searched = deque(sorted(searched, reverse=True, key=lambda s: s[1])[:beam_size])
             
             done = sorted(done, reverse=True, key=lambda s: s[1])
-            best = [word[0].item() for word in done[0][0]]
-            outputs.append(best)
+            if not done:
+                best = [word[0].item() for word in done[0][0]]
+                outputs.append(best)
         
         return outputs
                         
