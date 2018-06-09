4cbcf80b9e772f57046fb31c8272eaaad955f34d
Dave Chen <zhenyu.chen@campus.lmu.de>
2018-06-09 11:59:27 +0200

    modified beam search

--- decoders.py
+++ decoders.py
@@ -98,6 +98,7 @@ class Decoder(nn.Module):
             
             if not done:
                 done = sorted(done, reverse=True, key=lambda s: s[1])
+                print(done)
                 best = [word[0].item() for word in done[0][0]]
                 outputs.append(best)
             else:
