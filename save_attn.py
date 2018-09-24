d018b24c595d9321d70954d47fec4b0dda37b110
Dave Zhenyu Chen <davech2y@outlook.com>
2018-09-24 17:13:57 +0200

    added vis tsne

--- save_attn.py
+++ save_attn.py
@@ -11,64 +11,77 @@ from torch.utils.data import Dataset, DataLoader
 from PIL import Image
 from lib.data_embedding import *
 from lib.configs import CONF
-from model.encoder_attn import AdaptiveEncoder
+from model.encoder_attn import SelfAttnShapeEncoder
 from scipy.ndimage import zoom
 import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
-def save_inters(model_id, weights, idx, path):
-    for i in range(len(weights)):
-        spatial_mask = weights[i][0][:512].view(8, 8, 8).data.cpu().numpy()
-        spatial_mask = zoom(spatial_mask, (8, 8, 8))
-        spatial_mask = (spatial_mask * 255).astype(np.uint8)
+def apply_attn(model_id, raw_mask):
+    spatial_mask = raw_mask.data.cpu().numpy()
+    mask_size = spatial_mask.shape[0]
+    up_scalar = int(64 / mask_size)
+    spatial_mask = zoom(spatial_mask, (up_scalar, up_scalar, up_scalar))
+    spatial_mask = (spatial_mask * 255).astype(np.uint8)
 
-        model_path = os.path.join(CONF.PATH.SHAPENET_ROOT.format(64), CONF.PATH.SHAPENET_NRRD.format(model_id[0], model_id[0]))
-        readdata, _ = nrrd.read(model_path)
+    model_path = os.path.join(CONF.PATH.SHAPENET_ROOT.format(64), CONF.PATH.SHAPENET_NRRD.format(model_id, model_id))
+    readdata, _ = nrrd.read(model_path)
 
-        inters = np.zeros((4, 64, 64, 64))
-        inters[0][spatial_mask != 0] = readdata[0][spatial_mask != 0]
-        inters[1][spatial_mask != 0] = readdata[1][spatial_mask != 0]
-        inters[2][spatial_mask != 0] = readdata[2][spatial_mask != 0]
-        inters[3][spatial_mask != 0] = readdata[3][spatial_mask != 0]
-        inters = inters.astype(np.uint8)
-        inters = np.swapaxes(inters, 1, 2)
-        inters = np.swapaxes(inters, 1, 3)
+    applied = np.zeros((4, 64, 64, 64))
+    applied[0][spatial_mask != 0] = readdata[0][spatial_mask != 0]
+    applied[1][spatial_mask != 0] = readdata[1][spatial_mask != 0]
+    applied[2][spatial_mask != 0] = readdata[2][spatial_mask != 0]
+    applied[3][spatial_mask != 0] = readdata[3][spatial_mask != 0]
+    applied = applied.astype(np.uint8)
+    applied = np.swapaxes(applied, 1, 2)
+    applied = np.swapaxes(applied, 1, 3)
 
-        filename = os.path.join(path, "{}.nrrd".format(i))
-        nrrd.write(filename, inters)
+    return applied
 
-def extract(encoder, dataloader, root):
-    tempo_attn = {}
-    model_count = {}
-    for idx, (model_id, shape, text, _, _, _) in enumerate(dataloader):
-        shape = shape.cuda()
-        text = text.cuda()
-        _, _, weights, attn_mask = encoder(shape, text)
-        if model_id[0] in model_count.keys():
-            model_count[model_id[0]] += 1
-        else:
-            model_count[model_id[0]] = 1
-        tempo_attn["{}-{}".format(model_id[0], model_count[model_id[0]])] = (attn_mask[0][0].data.cpu().numpy(), text.data.cpu().numpy())
-        # print("extracted mask: {}-{}, {} left".format(model_id[0], model_count[model_id[0]], len(dataloader) - idx - 1))
+def filter_attn(model_ids, attn_masks):
+    filtered = []
+    for i in range(len(model_ids)):
+        raw_mask_1 = attn_masks[0][1][i].view(8, 8, 8)
+        raw_mask_2 = attn_masks[1][1][i].view(4, 4, 4)
+        applied_1 = apply_attn(model_ids[i], raw_mask_1)
+        applied_2 = apply_attn(model_ids[i], raw_mask_2)
 
-        # get attended part
-        path = os.path.join(root, "attention", "{}-{}".format(model_id[0], model_count[model_id[0]]))
-        if not os.path.exists(path):
-            os.mkdir(path)
-            save_inters(model_id, weights, idx, path)
-            print("extracted and saved: {}-{}".format(model_id[0], model_count[model_id[0]]))
-        else:
-            print("exists and skipped: {}-{}".format(model_id[0], model_count[model_id[0]]))
+        if applied_1.max() != 0 and applied_2.max() != 0:
+            filtered.append(
+                (
+                    model_ids[i],
+                    (
+                        applied_1,
+                        applied_2
+                    )
+                )
+            )
 
-    return tempo_attn
+    return filtered
 
+def save_attn(encoder, dataloader, root):
+    filtered_mask_list = []
+    for model_ids, shapes, _, _, _, _ in dataloader:
+        shapes = shapes.cuda()
+        _, attn_masks = encoder(shapes)
+        temp_list = filter_attn(model_ids, attn_masks)
+
+        for (model_id, (mask_1, mask_2)) in temp_list:
+            if not os.path.exists(os.path.join(root, "vis", "{}".format(model_id))):
+                os.mkdir(os.path.join(root, "vis", "{}".format(model_id)))
+            nrrd.write(os.path.join(root, "vis", "{}".format(model_id), "0.nrrd"), mask_1)
+            nrrd.write(os.path.join(root, "vis", "{}".format(model_id), "1.nrrd"), mask_2)
+
+        filtered_mask_list.extend(temp_list)
+
+    return filtered_mask_list
 
 def main(args):
     # parse args
     root = os.path.join(CONF.PATH.OUTPUT_EMBEDDING, args.path)
     voxel = int(args.path.split("_")[1][1:])
-    encoder_path = os.path.join(root, "models/encoder.pth")
+    attn_type = args.path.split("_")[-1]
+    encoder_path = os.path.join(root, "models/shape_encoder.pth")
     
     phase = args.phase
     size = args.size
@@ -83,7 +96,7 @@ def main(args):
     phase2idx = {'train': 0, 'val': 1, 'test': 2}
     size_split = [-1] * 3
     size_split[phase2idx[phase]] = size
-    shapenet = Shapenet(
+    data = Embedding(
         [
             pickle.load(open("data/shapenet_split_train.p", 'rb')),
             pickle.load(open("data/shapenet_split_val.p", 'rb')),
@@ -93,46 +106,38 @@ def main(args):
         1,
         False
     )
-    dataset = ShapenetDataset(
-        getattr(shapenet, "{}_data".format(phase)), 
-        getattr(shapenet, "{}_idx2label".format(phase)), 
-        getattr(shapenet, "{}_label2idx".format(phase)), 
+    dataset = EmbeddingDataset(
+        getattr(data, "{}_data".format(phase)), 
+        getattr(data, "{}_idx2label".format(phase)), 
+        getattr(data, "{}_label2idx".format(phase)), 
         voxel,
-        h5py.File(CONF.PATH.SHAPENET_DATABASE.format(voxel), "r")
+        h5py.File(CONF.PATH.SHAPENET_DATABASE.format(voxel), "r"),
+        aggr_shape=True
     )
-    dataloader = DataLoader(dataset, batch_size=1, shuffle=False, collate_fn=collate_shapenet)
+    dataloader = DataLoader(dataset, batch_size=100, shuffle=False, collate_fn=collate_embedding)
 
     # report settings
     print("[settings]")
     print("extract attention masks from {} set".format(phase))
-    print("size:", len(getattr(shapenet, "{}_data".format(phase))))
+    print("size:", len(dataset))
     print("gpu:", gpu)
 
     # initialize models
     print("\ninitializing models...\n")
-    encoder = AdaptiveEncoder(shapenet.dict_idx2word.__len__(), args.path.split("_")[-1][8:]).cuda()
+    encoder = SelfAttnShapeEncoder(attn_type, is_final=True).cuda()
     encoder.load_state_dict(torch.load(encoder_path))
     encoder.eval()
 
     # feed and save as nrrd
     print("start extracting...\n")
-    if not os.path.exists(os.path.join(root, "attention")):
-        os.mkdir(os.path.join(root, "attention"))
-    tempo_attn = extract(encoder, dataloader, root)
+    if not os.path.exists(os.path.join(root, "vis")):
+        os.mkdir(os.path.join(root, "vis"))
+    filtered_mask_list = save_attn(encoder, dataloader, root)
 
-    # dump
-    print("save attention mask...")
-    pickle.dump(tempo_attn, open(os.path.join(root, "attention", "attn_mask.p"), 'wb'))
+    # report
+    print("saved attention masks:", len(filtered_mask_list))
+    print("abandoned attention masks:", len(dataset) - len(filtered_mask_list))
 
-    # # render nrrd
-    # print("start rendering...\n")
-    # render(root)
-
-    # # plot
-    # print("start generating summaries...")
-    # if not os.path.exists(os.path.join(root, "vis")):
-    #     os.mkdir(os.path.join(root, "vis"))
-    # summarize(root, tempo_attn, shapenet.dict_idx2word)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
